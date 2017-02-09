008d2c774c91b1da939c121c8e3e42a43ad187d3
UFreedom <sunfreedom@sina.cn>
2017-02-09 16:52:20 +0800

    Init demo

--- demo/src/main/java/com/ufreedom/demo/MainActivity.java
+++ demo/src/main/java/com/ufreedom/demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.ufreedom.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ufreedom.smartdialog.MaterialSmartDialog;
+import com.ufreedom.smartdialog.transition.ScaleTransition;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void showDialog(View view){
+        MaterialSmartDialog materialSmartDialog = new MaterialSmartDialog();
+        materialSmartDialog.setDialogTransition(new ScaleTransition());
+        materialSmartDialog.showDialog(this);
     }
 }
