8bdfa7a9e4908f2fe62c4e3ee690abfc63ae2962
UFreedom <sunfreedom@sina.cn>
2017-02-10 15:36:14 +0800

    Split Lib to 'core' and 'expand'

--- expand/src/main/java/com/ufreedom/smartdialog/BottomSheetDialog.java
+++ expand/src/main/java/com/ufreedom/smartdialog/BottomSheetDialog.java
@@ -3,20 +3,21 @@ package com.ufreedom.smartdialog;
 import android.view.View;
 
 /**
- * Author SunMeng
- * Date : 2017 二月 09
+ * Author UFreedom
+ * Date : 2017 二月 10
  */
 
-public class MaterialSmartDialog extends BaseDialog {
-    
+public class BottomSheetDialog extends BaseDialog {
+
     @Override
     protected void onInitView(View dialog) {
-        
+
     }
 
     @Override
     protected int getDialogContentLayoutResId() {
-        return R.layout.smart_dialog_material_style;
+        return 0;
     }
-    
+
+
 }
---
+++ expand/src/main/java/com/ufreedom/smartdialog/MaterialSmartDialog.java
@@ -0,0 +1,22 @@
+package com.ufreedom.smartdialog;
+
+import android.view.View;
+
+/**
+ * Author SunMeng
+ * Date : 2017 二月 09
+ */
+
+public class MaterialSmartDialog extends BaseDialog {
+
+    @Override
+    protected void onInitView(View dialog) {
+
+    }
+
+    @Override
+    protected int getDialogContentLayoutResId() {
+        return R.layout.smart_dialog_material_style;
+    }
+
+}
